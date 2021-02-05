4c1201924aed01a9c36b192fd9b8d5436449d87c
Andrey <15253861+forem1@users.noreply.github.com>
2021-02-05 13:22:42 +0300

    Add files via upload

---
+++ testing/MyCalcLib/main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalcLib
+{
+    public class main
+    {
+        public int Summ(int x, int y)
+        {
+            return x + y;
+        }
+        public int del(int x, int y)
+        {
+            return x / y;
+        }
+        public int minus(int x, int y)
+        {
+            return x - y;
+        }
+        public int multi(int x, int y)
+        {
+            return x * y;
+        }
+        public int sqr(int x)
+        {
+            return (int)Math.Sqrt(x);
+        }
+        public int xtwo(int x)
+        {
+            return x*x;
+        }
+    }
+
+    /*сложение вычитание умножение деление корень квадрат
+     * 
+     * 
+     */
+}
---
+++ testing/driven/UserData/User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserData
+{
+    public class User
+    {
+        public bool Add(string userId, string phone, string email, string sex, int passport)
+        {
+            if (userId.Length < 4)
+            {
+                throw new Exception("UserId должен быть больше 4 символов");
+            }
+
+            if (phone.Contains("a"))
+            {
+                throw new Exception("Телефон должен содержать только цифры");
+            }
+
+            if (!email.Contains("@"))
+            {
+                throw new Exception("Ошибка в email адресе");
+            }
+            
+            if (!sex.Contains("male"))
+            {
+                throw new Exception("Ошибка в указании пола");
+            }
+
+            if (passport < 10)
+            {
+                throw new Exception("Ошибка в указании пасспорта");
+            }
+
+            return true;
+
+        }
+    }
+}
